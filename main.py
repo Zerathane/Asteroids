6821b8265f30ae538e7fb5a233232325ef0e04be
Zerathane <mason2808@hotmail.co.uk>
2026-04-26 19:49:50 +0100

    Add player movement logic

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import pygame
 from constants import *
 from logger import log_state
+from player import *
 
 def main():
     version = pygame.version.ver
@@ -9,6 +10,11 @@ def main():
     print(f"Screen height: {SCREEN_HEIGHT}")
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0
+    x = SCREEN_WIDTH / 2
+    y = SCREEN_HEIGHT / 2
+    player = Player(x, y)
     
     while True:
         log_state()
@@ -16,8 +22,12 @@ def main():
             if event.type == pygame.QUIT:
                 return
         screen.fill("black")
+        player.update(dt)   
+        player.draw(screen)
         pygame.display.flip()
-
+        dt = clock.tick(60) / 1000
+        
+        
 
 if __name__ == "__main__":
     main()
