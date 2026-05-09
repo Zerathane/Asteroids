d75d2c4e519111a6c6bf4ddfcb1f4c1bb4548e84
Zerathane <mason2808@hotmail.co.uk>
2026-05-09 18:04:37 +0100

    Add kill method to shot, tweak shot size and speed to align more with original

--- shot.py
+++ shot.py
@@ -4,12 +4,16 @@ from constants import *
 class Shot(CircleShape):
     def __init__(self, x, y, radius):
         super().__init__(x, y, radius)
+        self.lifetime = SHOT_LIFETIME_SECONDS
     
     def draw(self, screen):
         pygame.draw.circle(screen, "white", self.position, self.radius)
 
     def update(self, dt):
         self.position += (self.velocity * dt)
+        self.lifetime -= dt
+        if self.lifetime <= 0:
+            self.kill()
         self.wrap(SCREEN_WIDTH, SCREEN_HEIGHT)
 
     
